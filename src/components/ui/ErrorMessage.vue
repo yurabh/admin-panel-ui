c7c76726aa79516a04d0e12f065cf4a5895c9bd0
Yuriy <yurabh1990@gmail.com>
2026-06-14 00:10:26 +0300

    Add ErrorMessage.vue component for display error

---
+++ src/components/ui/ErrorMessage.vue
@@ -0,0 +1,21 @@
+<template>
+  <p v-if="message" class="error-message">
+    {{ message }}
+  </p>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  message?: string | null
+}
+
+defineProps<Props>()
+</script>
+
+<style scoped>
+.error-message {
+  color: rgba(235, 37, 166, 0.98);
+  font-size: 0.875rem;
+  margin-bottom: 1rem;
+}
+</style>
