4b72c55db71306549311d1c28fafd37d570a1115
Yuriy <yurabh1990@gmail.com>
2026-07-14 11:06:57 +0300

    Add SuccessMessage.vue

---
+++ src/components/ui/SuccessMessage.vue
@@ -0,0 +1,24 @@
+<template>
+  <p v-if="message" class="success-message">
+    {{ message }}
+  </p>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  message?: string | null
+}
+
+defineProps<Props>()
+</script>
+
+<style scoped>
+.success-message {
+  color: #166534;
+  background-color: #dcfce7;
+  padding: 0.75rem 1rem;
+  border-radius: 6px;
+  font-size: 0.875rem;
+  margin-bottom: 1rem;
+}
+</style>
